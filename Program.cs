669723a9153cbcd8f29b1d0392051c527c28d00f
unknown <krntranslation@gmail.com>
2019-09-30 10:47:52 -0500

    Created new program Lemonade, added a random weather generator

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weather weather = new Weather();
+            weather.WhatsTheWeather();
+        }
+    }
+}
---
+++ Weather.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemonade
+{
+    class Weather
+    {   //Member Variables
+        string temperature;
+        string cloudy;
+        string sunny;
+        string raining;
+        string todaysWeather;
+        //Constructor
+        public Weather()
+        {
+            temperature = null;
+            cloudy = null;
+            sunny = null;
+            raining = null;
+            todaysWeather = null;
+
+
+            List<string> weatherList = new List<string>() { "Temp-89 degrees,\n cloudy", "Temp-66 degrees, \n sunny", "Temp-99 degrees, \n raining", "Temp-60 degrees, \n cloudy", "Temp-90 degrees, \n sunny", "Temp-75 degrees, \n raining", "Temp-87 degrees, \n sunny" };
+        }
+        //Member Methods
+        public void WhatsTheWeather()
+        {
+            var random = new Random();
+            var list = new List<string>() { "Temp-89 degrees,\ncloudy", "Temp-66 degrees, \nsunny", "Temp-99 degrees, \nraining", "Temp-60 degrees, \ncloudy", "Temp-90 degrees, \nsunny", "Temp-75 degrees, \nraining", "Temp-87 degrees, \nsunny" };
+            int index = random.Next(list.Count);
+            Console.WriteLine(list[index]);
+            Console.ReadLine();
+
+        }
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
