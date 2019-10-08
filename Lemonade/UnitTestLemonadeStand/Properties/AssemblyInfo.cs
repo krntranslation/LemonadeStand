69d6535abdde9f14d3981b1e14f7eaf6e91fdecb
unknown <krntranslation@gmail.com>
2019-10-08 16:04:31 -0500

    fixing repo structure

---
+++ Lemonade/UnitTestLemonadeStand/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLemonadeStand")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLemonadeStand")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42f0da05-71f9-492d-86f1-5e6150b45d1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lemonade/UnitTestLemonadeStand/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lemonade;
+namespace UnitTestLemonadeStand
+{
+    [TestClass]
+    public class UnitTestLemonadeStand
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange = Setting up variable may need
+            
+
+            //Act = methods we are testing
+
+            //Assert = what we are determining
+        }
+    }
+}
